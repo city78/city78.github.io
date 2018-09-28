83afd218f1c4b4f64c9a93e3640afbc46c02f34f
Theo Go <tgoetemann@gmail.com>
2018-09-28 08:48:38 -0400

    a

---
+++ assets/js/ml1.js
@@ -0,0 +1,35 @@
+// Wrap every letter in a span
+$('.ml11 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 1000
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0,$(".ml11 .letters").width()],
+    easing: "easeOutExpo",
+    duration: 200,
+    delay: 0
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 300,
+    offset: '-=775',
+    delay: function(el, i) {
+      return 34 * (i+1)
+    }
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
---
+++ assets/js/user.js
@@ -0,0 +1,5 @@
+/* this part for jarallax */
+$('.jarallax').jarallax({
+    speed: 0.3,
+    noAndroid: true
+});
